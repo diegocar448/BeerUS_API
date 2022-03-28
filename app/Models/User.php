70f1b82eb7c0cee35c52c550bbfd56c464cb6cda
Diego Cardoso <diegocar448@hotmail.com>
2022-03-28 15:32:53 -0300

    API para App BeerUS

---
+++ app/Models/User.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class User
+ * 
+ * @property int $id
+ * @property string $email
+ * @property string $password
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $deleted_at
+ *
+ * @package App\Models
+ */
+class User extends Model
+{
+	use SoftDeletes;
+	protected $table = 'users';
+
+	protected $hidden = [
+		'password'
+	];
+
+	protected $fillable = [
+		'email',
+		'password'
+	];
+}
---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/', function () {
+    return response()->json(['message' => 'BeerUs Flutter API', 'status' => 'Connected']);
+});
+
+
+/* Route::group(['middleware' => 'api'], function ($router) {
+
+    Route::post('login', [ AuthController::class, 'login']);
+    Route::post('register', [ AuthController::class, 'register']);
+    Route::post('logout', [ AuthController::class , 'logout']);
+    Route::post('refresh', [ AuthController::class, 'refresh']);
+    Route::post('me', [ AuthController::class, 'me']);
+
+    Route::resource('companies', CompanyController::class)->except(['create', 'edit']);
+    Route::resource('employees', EmployeeController::class)->except(['create', 'edit']);
+    Route::resource('schedules', SchedulesController::class)->except(['create', 'edit']);
+    Route::resource('services', ServiceController::class)->except(['create', 'edit']);
+
+    Route::get('/', function () {
+        return response()->json(['message' => 'Barber Flutter API', 'status' => 'Connected']);
+    });
+
+}); */
